1f327549a574e01864459382d798c8aa9de8d012
LucThao <lucthaontvx@gmail.com>
2019-11-23 17:40:07 +0700

    commit code by thao

--- routes/web.php
+++ routes/web.php
@@ -20,11 +20,6 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/survey', 'SurveyController@index')->name('survey');
 Route::get('/qna', 'QnAController@index')->name('qna');
-<<<<<<< HEAD
-Route::post('/qna', 'QnAController@create')->name('addSession');
-Route::post('/qna/del', 'QnAController@delete')->name('deleteSession');
-=======
 Route::post('/qna/add', 'QnAController@create')->name('addSession');
 Route::post('/qna/del', 'QnAController@delete')->name('deleteSession');
 Route::post('/qna/edit', 'QnAController@edit')->name('editSession');
->>>>>>> cf53403c65e2a93ad6ffa384b030fd52bac30023
