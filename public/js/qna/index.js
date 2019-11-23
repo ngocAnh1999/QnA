1d7f86eee292b342340b3463eea761dca406f6a8
LucThao <lucthaontvx@gmail.com>
2019-11-23 17:33:51 +0700

    commit code by thao

--- public/js/qna/index.js
+++ public/js/qna/index.js
@@ -1,12 +1,21 @@
 
 function validate(me) {
-debugger
 	if(me.value != "") {
-		
-		document.getElementById("btn btn-success").removeAttribute("disabled");
+		me.value = me.value.toLowerCase();
+		me.value = me.value.trim();
+		me.value.charAt(0).toUpperCase() + me.value.slice(1);
+
+		document.getElementById("btn-add").removeAttribute("disabled");
     }
     else {
     debugger
-    document.getElementById("btn btn-success").setAttribute("disabled", "disabled");
+    document.getElementById("btn-add").setAttribute("disabled", "disabled");
     }
+}
+
+function jsUcfirst(name) // chuyển hết thành chữ thường, cắt khoảng trắng đầu cuối rồi viết hoa chữ cái đầu tiên
+{
+	string = string.toLowerCase();
+	string = string.trim();
+	return string.charAt(0).toUpperCase() + string.slice(1);
 }
--- resources/views/survey/index.blade.php
+++ resources/views/survey/index.blade.php
@@ -1,44 +1,53 @@
 @extends('layouts.app')
-
 @section('title', 'Phiên khảo sát')
-
 @section('content')
 @push('styles')
     <link href="{{ asset('css/survey/indexSurvey.css') }}" rel="stylesheet">
 @endpush
 <div class="body-content">
-    <div class="row toolbar font-weight-bolder border-bottom">
-        <div class="col-sm-8 h-100">
-            <div class="row h-100">
-                <ul class="nav nav-tabs">
-                    <li class="nav-item">
-                        <a class="nav-link active" href="./indexSurvey.html">Phiên của tôi</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link " href="./createSurvey.html">Tạo phiên khảo sát</a>
-                    </li>
-                </ul>
-            </div>
+    <div class="row toolbar border-bottom d-flex justify-content-between align-content-center px-0">
+        <div class="form-group">
+            <select class="btn list-session h-100 shadow">
+                <option class="m-8" value="all" >Tất cả các phiên</option>
+                <option class="m-8" value="activated" >Phiên đang hoạt động</option>
+                <option class="m-8" value="closed" >Phiên đã đóng</option>
+                <option class="m-8" value="my-session" selected>Các phiên của tôi</option>
+            </select>
         </div>
-        <div class="col-md"></div>
-
+            <form class="search-cls" action="#" method="post">
+                @csrf
+                    <input class="h-100" name="search" placeholder="Tìm kiếm" />
+                    <button class="ml-2 px-4 btn btn-success h-100" type="submit"><span class="glyphicon glyphicon-search"></span></button>
+            </form>
     </div>
-    <div class="row content-header">
-        <h3 id="survey_id" class="col-md text-info text-center">Phiên khảo sát</h3>
+    <div class="row px-4 py-2">
+        <button data-toggle="modal" data-target="#addModal" class="btn btn-success rounded-circle">
+            <span class="glyphicon glyphicon-plus"></span>
+        </button>
+        @error('time_start')
+            <script>
+                alert("lỗi !!!!");
+            </script>
+        @enderror
     </div>
     <div class="row content mx-auto d-flex flex-column align-items-center">
-        <form action="" method="get" class="wrap bg-white m-4 wrap-content shadow mb-8">
-            <h3 name="survey-name" property="survey_1">Tên phiên</h3>
-            <p > Mô tả: </p>
-            <p > Người tạo: </p>
-            <p > Thời gian: </p>
-            <div class=" d-flex justify-content-end">
-                <button formaction="./answerSurvey.html" type="submit" class="btn btn-primary mr-2">Trả lời</button>
-                <button formaction="#" type="submit" class="btn btn-primary mr-2">Thống kê</button>
-                <button type="button" onclick="javascript:deleteSurvey(this)" class="btn btn-primary" data-toggle="modal" data-target="#modalDelete">Xóa</button>
-            </div>
-        </form>
-        
+        @empty($sessions)
+            
+        <div class="t_max text-center">Chưa có phiên nào!</div>
+        @endempty
+        <table class="table-bordered">
+            <thead>
+                <tr class="bg-dark text-white">
+                    <th scope="col">STT</th>
+                    <th scope="col">Session name</th>
+                    <th scope="col">Time start</th>
+                    <th scope="col">Time end</th>
+                    <th scope="col">Status</th>
+                    <th>Edit</th>
+                    <th>Delete</th>
+                </tr>
+            </thead>
+        </table>
     </div>
 </div>
 
